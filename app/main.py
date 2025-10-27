ba37fbc749cdb7c11ad3bda185c34719a07b67c3
Nikita Grinyov <nikita3grynov@gmail.com>
2025-10-27 22:02:37 +0200

    changes

--- app/main.py
+++ app/main.py
@@ -29,7 +29,8 @@ class CarWashStation:
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
         total_score = self.average_rating * self.count_of_ratings
